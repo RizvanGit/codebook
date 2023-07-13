2ef7cfe5dde4b161da9338eb8aed47a8bec9151b
Rizvan <imeandmyep2@gmail.com>
2023-07-13 19:31:12 +0300

    split onLoad methods

--- src/plugins/fetch-plugin.ts
+++ src/plugins/fetch-plugin.ts
@@ -9,23 +9,15 @@ export const fetchPlugin = (inputCode: string) => {
   return {
     name: "fetch-plugin",
     setup(build: esbuild.PluginBuild) {
-      //Handle main file of a module
-      build.onResolve({ filter: /.*/ }, async (args: any) => {
+      build.onLoad({ filter: /(^index\.js$)/ }, () => {
         return {
-          namespace: "a",
-          path: `https://unpkg.com/${args.path}`,
+          loader: "jsx",
+          contents: inputCode,
         };
       });
-
-      build.onLoad({ filter: /.*/ }, async (args: any) => {
+      build.onLoad({ filter: /.css$/ }, async (args: any) => {
         console.log("onLoad", args);
 
-        if (args.path === "index.js") {
-          return {
-            loader: "jsx",
-            contents: inputCode,
-          };
-        }
         //Check if we already fetched this file
         const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
           args.path,
@@ -35,17 +27,12 @@ export const fetchPlugin = (inputCode: string) => {
         }
 
         const { data, request } = await axios.get(args.path);
-        const fileType = args.path.match(/.css$/) ? "css" : "jsx";
-        console.log("file type ", fileType);
 
-        const contents =
-          fileType === "css"
-            ? `
+        const contents = `
           const style = document.createElement('style');
           style.innerText = \`${data}\`;
           document.head.appendChild(style);
-        `
-            : data;
+        `;
         //store response in indexedDB
         const result: esbuild.OnLoadResult = {
           loader: "jsx",
