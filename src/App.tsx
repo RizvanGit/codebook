6bf08817fbac04a658f2b903cfe71def03780e62
Rizvan <imeandmyep2@gmail.com>
2023-07-20 19:10:41 +0300

    text editor MDEditor

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import { FC } from "react";
 import "./App.css";
 import "bulmaswatch/superhero/bulmaswatch.min.css";
-import CodeCell from "./components/CodeCell/CodeCell";
-
+//import CodeCell from "./components/CodeCell/CodeCell";
+import TextEditor from "./components/TextEditor/TextEditor";
 const App: FC = () => {
-  return <CodeCell />;
+  return (
+    <>
+      <TextEditor />
+    </>
+  );
 };
 
 export default App;
---
+++ src/components/TextEditor/TextEditor.tsx
@@ -0,0 +1,38 @@
+import { FC, useState, useEffect, useRef } from "react";
+import MDEditor from "@uiw/react-md-editor";
+
+const TextEditor: FC = () => {
+  const [isEditing, setIsEditing] = useState(false);
+  const editorRef = useRef<HTMLElement | null>(null);
+  useEffect(() => {
+    const listener = (event: MouseEvent) => {
+      if (
+        event.target &&
+        editorRef.current &&
+        editorRef.current.contains(event.target as Node)
+      ) {
+        return;
+      }
+      setIsEditing(false);
+    };
+    document.addEventListener("click", listener, { capture: true });
+
+    return () => {
+      document.removeEventListener("click", listener, { capture: true });
+    };
+  }, []);
+  if (isEditing) {
+    return (
+      <section ref={editorRef}>
+        <MDEditor />
+      </section>
+    );
+  }
+  return (
+    <section onClick={() => setIsEditing(true)}>
+      <MDEditor.Markdown source={"# Header"} />
+    </section>
+  );
+};
+
+export default TextEditor;
