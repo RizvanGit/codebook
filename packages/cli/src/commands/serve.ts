4117529cd7e01a14722b16a5ecf6609c6933baf7
Rizvan <imeandmyep2@gmail.com>
2023-07-27 19:20:35 +0300

    Error handling when running express and serve command

--- packages/cli/src/commands/serve.ts
+++ packages/cli/src/commands/serve.ts
@@ -3,13 +3,40 @@ import path from "path";
 import { Command } from "commander";
 import { serve } from "local-api";
 
+interface ILocalApiError {
+  code: string;
+}
 //angle brackets for required value
 //square brackets for optional value
 export const serveCommand = new Command()
   .command("serve [filename]")
   .description("Open a file for editing")
   .option("-p, --port <number>", "port to run server on", "4005")
-  .action((filename = "notebook.js", options: { port: string }) => {
-    const dir = path.join(process.cwd(), path.dirname(filename));
-    serve(parseInt(options.port), path.basename(filename), dir);
+  .action(async (filename = "notebook.js", options: { port: string }) => {
+    const isLocalApiError = (err: any): err is ILocalApiError => {
+      return typeof err.code === "string";
+    };
+    try {
+      const dir = path.join(process.cwd(), path.dirname(filename));
+      await serve(parseInt(options.port), path.basename(filename), dir);
+      console.log(
+        `
+         Opened ${filename}.
+         Navigate to http://localhost:${options.port} to edit the file
+        `,
+      );
+    } catch (error) {
+      if (isLocalApiError(error)) {
+        if (error.code === "EADDRINUSE") {
+          console.log(
+            `Port ${options.port} is in use. Try running different port: --port`,
+          );
+        }
+      } else if (error instanceof Error) {
+        console.log("Error: ", error.message);
+      } else {
+        console.log(error);
+      }
+      process.exit(1);
+    }
   });
